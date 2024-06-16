2d5634afb32b2836dc9f70bac626242e989d2229
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-06-16 14:51:54 -0500

    Time: 67 ms (54.98%) | Memory: 16.7 MB (37.54%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+
+        fill(image, sr,sc,color,image[sr][sc])
+        return image
+
+def fill(image, sr, sc, color, cur):
+    if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[0]):
+        return
+    if cur != image[sr][sc]:
+        return
+    if image[sr][sc] == color:
+        return
+
+    
+    image[sr][sc] = color
+    
+    fill(image, sr-1,sc,color,cur)
+    fill(image, sr+1,sc,color,cur)
+    fill(image, sr,sc-1,color,cur)
+    fill(image, sr,sc+1,color,cur)

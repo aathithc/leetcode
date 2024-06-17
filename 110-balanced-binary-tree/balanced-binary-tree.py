8cb3407bdfdc79a568a07b426788577aaaaa3924
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-06-17 14:26:32 -0500

    Time: 39 ms (88.29%) | Memory: 17.7 MB (41.37%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -9,13 +9,12 @@ class Solution:
         def dfs(root):
             if not root:
                 return [True, 0]
-            
             left = dfs(root.left)
             right = dfs(root.right)
-            
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            height = 1 + max(left[1], right[1])
-            
+
+            balanced = left[0] and right[0] and abs(left[1]-right[1]) <= 1
+            height = 1+ max(left[1],right[1])
+
             return [balanced, height]
-        
         return dfs(root)[0]
+        

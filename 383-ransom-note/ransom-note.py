961c7efe2b636a117ca0b8a5b3a53fa65f9e65f0
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-06-18 13:30:57 -0500

    Time: 56 ms (60.27%) | Memory: 16.7 MB (35.93%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,7 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        a = set(ransomNote) 
+        a = set(ransomNote)
+
         for i in a:
             if magazine.count(i) < ransomNote.count(i):
-              return False
+                return False
         return True

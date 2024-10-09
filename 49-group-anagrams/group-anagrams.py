e4b7688744b6f70296e8740b1254078efb96d19a
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-09 16:43:24 -0400

    Time: 95 ms (34.81%) | Memory: 22.2 MB (19.54%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,12 @@
-class Solution(object):
-    def groupAnagrams(self, strs):
-        map = collections.defaultdict(list) #map chat count of each string(key) to list of anagrams (ensures that default value is 0 edge case if it doesnt exist)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        map = collections.defaultdict(list)
 
         for s in strs:
+            
             count = [0] * 26
 
             for c in s:
-                count[ord(c)-ord("a")] += 1
-                
-            map[tuple(count)].append(s) #for this count append string s
+                count[ord(c) - ord("a")] += 1
+            map[tuple(count)].append(s)
         return map.values()
-
-
-        

c8c42479849827df04a1cc0a770f053c6bf6f836
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-09 17:06:53 -0400

    Time: 97 ms (17.57%) | Memory: 22 MB (16.76%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,19 +1,17 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        count = {}
-        frequency = [[] for i in range(len(nums) + 1)]
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count ={}
+        freq = [[] for i in range(len(nums) + 1)]
 
         for n in nums:
-            count[n] = 1 + count.get(n,0)
+            count[n] = 1 + count.get(n, 0)
         
         for n,c in count.items():
-            frequency[c].append(n)
+            freq[c].append(n)
         
-        res=[]
-        for i in range(len(frequency)-1,0,-1):
-            for n in frequency[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
-
-        # O(n)
+        res = []
+        
+        for i in range(len(freq) - 1, 0, -1):
+            res += freq[i]
+            if len(res) == k:
+                return res

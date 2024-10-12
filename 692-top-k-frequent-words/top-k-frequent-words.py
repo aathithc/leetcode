23d0f7ebb82bb293535742aae35fa12d24cd9ac5
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-11 20:53:14 -0400

    Time: 60 ms (41.38%) | Memory: 16.6 MB (93.53%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.py
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -1,21 +1,22 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        count ={}
+        count = {}
         freq = [[] for i in range(len(words) + 1)]
 
-        for n in words:
-            count[n] = 1 + count.get(n, 0)
+        for c in words:
+            count[c] = 1 + count.get(c, 0)
         
-        for n,c in count.items():
+        for n, c in count.items():
             freq[c].append(n)
         
         for c in freq:
             c.sort()
-            
+
         res = []
-        
-        for i in range(len(freq) - 1, 0, -1):
-            for c in freq[i]:
-                res.append(c)
+
+        for i in range(len(freq) -1, 0, -1):
+            for word in freq[i]:
+                res.append(word)
                 if len(res) == k:
                     return res
+        

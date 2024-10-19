fddc6f5bf72dc372bf353187bb848586bece2977
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-19 00:38:04 -0400

    Time: 89 ms (100.00%) | Memory: 56.2 MB (100.00%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -10,6 +10,7 @@ class RandomizedSet:
             self.numMap[val] = len(self.numList)
             self.numList.append(val)
         return res
+
     def remove(self, val: int) -> bool:
         res = val in self.numMap
         if res:
@@ -23,6 +24,7 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.numList)
+        
 
 
 # Your RandomizedSet object will be instantiated and called as such:

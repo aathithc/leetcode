867dab7945c2a90f0a44aa92941d282b0b92832a
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-19 01:15:32 -0400

    Time: 114 ms (100.00%) | Memory: 56.2 MB (100.00%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -3,7 +3,6 @@ class RandomizedSet:
     def __init__(self):
         self.numMap = {}
         self.numList = []
-        
 
     def insert(self, val: int) -> bool:
         res = val not in self.numMap
@@ -11,7 +10,6 @@ class RandomizedSet:
             self.numMap[val] = len(self.numList)
             self.numList.append(val)
         return res
-
     def remove(self, val: int) -> bool:
         res = val in self.numMap
         if res:

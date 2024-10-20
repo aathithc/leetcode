ed277479cb2cfc6d1262ce0ae1cbdc66af974a7f
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-10-20 03:10:13 -0400

    Time: 28 ms (99.13%) | Memory: 29.6 MB (5.32%) - LeetSync

--- 1512-design-underground-system/design-underground-system.py
+++ 1512-design-underground-system/design-underground-system.py
@@ -1,12 +1,11 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.checkInMap = {} # id -> (startStation, time)
-        self.totalMap = {} # (start, end) -> [totaltime, count]
+        self.checkInMap = {} # id -> (start, t)
+        self.totalMap = {} # (start, end) -> [time, count]
 
     def checkIn(self, id: int, startStation: str, t: int) -> None:
         self.checkInMap[id] = (startStation, t)
-
 
     def checkOut(self, id: int, endStation: str, t: int) -> None:
         start, time = self.checkInMap[id]
@@ -15,10 +14,10 @@ class UndergroundSystem:
             self.totalMap[route] = [0, 0]
         self.totalMap[route][0] += t - time
         self.totalMap[route][1] += 1
-
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         total, count = self.totalMap[(startStation, endStation)]
-        return total / count 
+
+        return total / count
 
 
 # Your UndergroundSystem object will be instantiated and called as such:

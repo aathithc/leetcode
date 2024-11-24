1e08f628adf5f59c99b9d9074f277e90543ed60f
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-11-24 16:55:53 -0500

    Time: 46 ms (41.74%) | Memory: 21.4 MB (42.69%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -6,11 +6,10 @@ class Solution:
             if stack and stack[-1][0] == c:
                 stack[-1][1] += 1
             else:
-                stack.append([c,1])
+                stack.append([c, 1])
             if stack[-1][1] == k:
                 stack.pop()
         res = ""
         for char, count in stack:
             res += (char * count)
         return res
-            

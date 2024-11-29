ca34b867514119bfa4207d139170bedfc3f6324a
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-11-29 00:59:54 -0500

    Time: 1 ms (64.71%) | Memory: 17.4 MB (5.02%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,12 +1,30 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        merged = nums1 + nums2
-        merged.sort()  # Sorting the merged array
-        
-        # Step 2: Find the median
-        n = len(merged)
-        if n % 2 == 1:  # Odd length
-            return float(merged[n // 2])  # Middle element
-        else:  # Even length
-            mid1, mid2 = merged[n // 2 - 1], merged[n // 2]
-            return (mid1 + mid2) / 2.0        
+        A, B = nums1, nums2
+        total = len(nums1) + len(nums2)
+        half = total // 2
+
+        if len(B) < len(A):
+            A, B = B, A
+
+        l, r = 0, len(A) - 1
+        while True:
+            i = (l + r) // 2  # Partition for A
+            j = half - i - 2  # Partition for B
+
+            Aleft = A[i] if i >= 0 else float("-infinity")
+            Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
+            Bleft = B[j] if j >= 0 else float("-infinity")
+            Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
+
+            # Partition is correct
+            if Aleft <= Bright and Bleft <= Aright:
+                # Odd total length
+                if total % 2:
+                    return min(Aright, Bright)
+                # Even total length
+                return (max(Aleft, Bleft) + min(Aright, Bright)) / 2
+            elif Aleft > Bright:
+                r = i - 1
+            else:
+                l = i + 1

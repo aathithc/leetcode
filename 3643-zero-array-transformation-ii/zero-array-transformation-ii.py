f9f7ce1488fa7d024e50f9f413254c98255da30b
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-12-03 00:38:52 -0500

    Time: 1252 ms (30.81%) | Memory: 64 MB (5.65%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+
+        def good(target):
+            line = [0 for i in range(len(nums) + 1)]
+
+            for start, end, val in queries[:target]:
+                line[start] += val
+                line[end + 1] -= val
+            
+            for i in range(1, len(line)):
+                line[i] += line[i - 1]
+            
+            for n in range(len(nums)):
+                if line[n] < nums[n]:
+                    return False
+            return True
+        
+        l, r = 0, len(queries) + 1
+        result = -1
+        while l < r:
+            m = (l + r) // 2
+            if good(m):
+                result = m
+                r = m 
+            else:
+                l = m + 1
+        return result if result != -1 else -1

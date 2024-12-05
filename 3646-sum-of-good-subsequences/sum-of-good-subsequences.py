35e8d1268abe304f618af5b898eb099323540a20
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-12-05 03:36:13 -0500

    Time: 1500 ms (19.09%) | Memory: 39.5 MB (11.32%) - LeetSync

--- 3646-sum-of-good-subsequences/sum-of-good-subsequences.py
+++ 3646-sum-of-good-subsequences/sum-of-good-subsequences.py
@@ -1,18 +1,18 @@
 class Solution:
     def sumOfGoodSubsequences(self, nums: List[int]) -> int:
-        dp = collections.defaultdict(lambda : [0, 0]) # num -> [count, sum]
+        dp = collections.defaultdict(lambda: [0, 0]) #num -> [count, sum]
         res = 0
         mod = 10 ** 9 + 7
+
         for n in nums:
             count1, sum1 = dp[n - 1]
             count2, sum2 = dp[n + 1]
 
             count = count1 + count2 + 1
-            total_sum = sum1 + sum2 + count * n
+            total = sum1 + sum2 + n * count
 
             dp[n][0] += count
-            dp[n][1] += total_sum
-
-            res += total_sum
+            dp[n][1] += total
+            res += total
             res = res % mod
         return res

69fdea48a10270884dc66a5fc022973743d8677c
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-12-05 18:31:12 -0500

    Time: 273 ms (75.15%) | Memory: 60.8 MB (66.06%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
@@ -3,14 +3,16 @@ class Solution:
         q = deque(sorted(queries))
         available = []
         working = []
+
         for i in range(len(nums)):
             while q and q[0][0] <= i:
                 heappush(available, -q.popleft()[1])
             while working and working[0] < i:
                 heappop(working)
+            
             while nums[i] > len(working):
                 if available and -available[0] >= i:
                     heappush(working, -heappop(available))
                 else:
                     return -1
-        return len(available)        
+        return len(available)

10fcf8386b137bd1742d8dd079121b377472951e
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-12-05 18:48:33 -0500

    Time: 263 ms (83.33%) | Memory: 60.8 MB (66.06%) - LeetSync

--- 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.py
@@ -7,8 +7,10 @@ class Solution:
         for i in range(len(nums)):
             while q and q[0][0] <= i:
                 heappush(available, -q.popleft()[1])
+            
             while working and working[0] < i:
                 heappop(working)
+            
             while nums[i] > len(working):
                 if available and -available[0] >= i:
                     heappush(working, -heappop(available))

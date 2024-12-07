148e0a4d2d553bb7d667229f5159f9ed56123771
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2024-12-06 20:56:07 -0500

    Time: 0 ms (100.00%) | Memory: 17.1 MB (13.19%) - LeetSync

--- 394-decode-string/decode-string.py
+++ 394-decode-string/decode-string.py
@@ -1,7 +1,6 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-
         for c in s:
             if c != "]":
                 stack.append(c)
@@ -12,6 +11,6 @@ class Solution:
                 stack.pop()
                 k = ""
                 while stack and stack[-1].isdigit():
-                    k = stack.pop() + k
+                    k  = stack.pop() + k
                 stack.append(int(k) * substr)
         return "".join(stack)

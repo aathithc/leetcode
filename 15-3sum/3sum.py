a72f3fa2a65b2f8fee8b6cb6e923d8b63af75368
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2025-02-07 17:07:33 -0500

    Time: 536 ms (74.42%) | Memory: 20.8 MB (36.87%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,20 +3,19 @@ class Solution:
         res = []
 
         nums.sort()
-
         for i, n in enumerate(nums):
             if i > 0 and n == nums[i-1]:
                 continue
             l, r = i + 1, len(nums) - 1
             while l < r:
-                threesum = n + nums[l] + nums[r]
-                if threesum > 0:
+                threesome = n + nums[l] + nums[r]
+                if threesome > 0:
                     r -= 1
-                elif threesum < 0:
+                elif threesome < 0:
                     l += 1
                 else:
                     res.append([n, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l-1] and l < r:
+                    while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res

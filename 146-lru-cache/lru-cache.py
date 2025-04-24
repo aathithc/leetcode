bd832b2210cfda41909b647f0fa22c16bf6ca77d
Aathith Chandra <133802188+aathithc@users.noreply.github.com>
2025-04-23 21:29:23 -0400

    Time: 164 ms (30.39%) | Memory: 78.4 MB (55.29%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,7 +1,8 @@
 class Node:
     def __init__(self, key, val):
         self.key, self.val = key, val
-        self.prev = self.next = None
+        self.next = self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -18,8 +19,7 @@ class LRUCache:
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
-        
+        node.prev, node.next = prev, nxt
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
@@ -37,8 +37,6 @@ class LRUCache:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
-        
-
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
